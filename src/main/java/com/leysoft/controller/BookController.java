3d97c6617b587b1ee6dcbfddc451646becfec48d
Will Leyton <will.leyton@rappi.com>
2018-07-11 11:03:14 -0500

    Added BookController

---
+++ src/main/java/com/leysoft/controller/BookController.java
@@ -0,0 +1,10 @@
+package com.leysoft.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = {"/book"})
+public class BookController {
+
+}
