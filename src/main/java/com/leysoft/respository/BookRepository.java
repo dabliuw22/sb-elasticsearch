761f61248004b9251fb33fefc48e223ef510c279
Will Leyton <will.leyton@rappi.com>
2018-07-12 15:14:05 -0500

    Added two methods

--- src/main/java/com/leysoft/respository/BookRepository.java
+++ src/main/java/com/leysoft/respository/BookRepository.java
@@ -1,5 +1,7 @@
 package com.leysoft.respository;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
 
@@ -7,9 +9,12 @@ import com.leysoft.document.Book;
 
 public interface BookRepository extends ElasticsearchCrudRepository<Book, String> {
 	
-	@Query(value = "{\"bool\": {\"must\": {\"term\" : {\"name\": \"?0\"}}}}")
-	public Iterable<Book> findByNameCustom(String name);
+	@Query(value = "{\"match\": {\"name\": \"?0\"}}")
+	public List<Book> findByNameCustom(String name);
 	
-	@Query(value = "{\"bool\": {\"must\": {\"term\": {\"author.name\": \"?0\"}}}}")
-	public Iterable<Book> findByAuthorName(String name);
+	@Query(value = "{\"match_all\": {}}")
+	public List<Book> findAllCustom();
+	
+	@Query(value = "{\"term\": {\"author.name\": \"?0\"}}")
+	public List<Book> findByAuthorName(String name);
 }
