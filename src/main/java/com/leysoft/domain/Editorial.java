8c588864c025b82327e17a51a8e638f945b13703
Will Leyton <will.leyton@rappi.com>
2018-07-13 15:20:21 -0500

    Added Editorial

---
+++ src/main/java/com/leysoft/domain/Editorial.java
@@ -0,0 +1,14 @@
+package com.leysoft.domain;
+
+public class Editorial {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
