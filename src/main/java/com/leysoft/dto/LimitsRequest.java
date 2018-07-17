e4fc9aae32314257d3603b53b026244100b9e1c1
Will Leyton <will.leyton@rappi.com>
2018-07-17 09:22:32 -0500

    Added LimitRequest

---
+++ src/main/java/com/leysoft/dto/LimitsRequest.java
@@ -0,0 +1,25 @@
+
+package com.leysoft.dto;
+
+public class LimitsRequest {
+	
+	private Integer inferior;
+	
+	private Integer superior;
+
+	public Integer getInferior() {
+		return inferior;
+	}
+
+	public void setInferior(Integer inferior) {
+		this.inferior = inferior;
+	}
+
+	public Integer getSuperior() {
+		return superior;
+	}
+
+	public void setSuperior(Integer superior) {
+		this.superior = superior;
+	}
+}
